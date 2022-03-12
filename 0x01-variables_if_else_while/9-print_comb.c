2f7fa3b104f1bb15191ca2564bab110533d658d5
MofuBulaya01 <mofubulaya01@gmail.com>
2022-03-11 18:36:59 -0800

    My first commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-int i;
+int n;
 
-for (i = 48; i < 58; i++)
+for (n = 48; n < 58; i++)
 {
-putchar(i);
-if (i != 57)
+putchar(n);
+if (n != 57)
 {
 putchar(',');
-putchar(',');
+putchar(' ');
 }
 }
 
