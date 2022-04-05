bc0466c18eb12ca5c192a0938e6cd2b863b997c3
MofuBulaya01 <mofubulaya01@gmail.com>
2022-04-05 11:31:26 -0700

    Static library libmy.a

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * puts2 - print alternating chars of string
+ * _puts - prints string
  * @str: string
  * Return: void
  */
 
-void puts2(char *str)
+void _puts(char *str)
 {
-int i;
+int i = 0;
 
-i = 0;
 while (str[i] != '\0')
 {
-if (i % 2 == 0)
 _putchar(str[i]);
 i++;
 }
+
 _putchar('\n');
 }
